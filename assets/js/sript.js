4bdec99c75cb6930a07373859ae073978f734a56
Ws2L <souza.weslley@aluno.ifsp.edu.br>
2025-04-29 17:48:02 -0300

    criando barra de pesquisa

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
     dropdownToggle.forEach((toggle) => {
         toggle.addEventListener('click', function() {
@@ -7,6 +7,38 @@ document.addEventListener('DOMContentLoaded', function(){
             parent.classList.toggle('active');
         })
     })
+    //Fechar o dropdown ao clicar fora
+    document.addEventListener('click', function (e) {
+        dropdownToggle.forEach((toggle) => {
+            const parent = toggle.closest('.dropdown-item');
+            if (!parent.contains(e.target)) {
+                parent.classList.remove('active');
+            }
+        })
+    })
+})
 
-    
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click', () => {
+    searchOverlay.style.display = 'flex';
+    setTimeout(() => {
+        searchOverlay.querySelector('.search-input').focus();
+    }, 100);
+});
+
+//fechar botão
+
+closeSearch.addEventListener('click', () => {
+    searchOverlay.style.display = 'none';
+})
+
+//fechar com o ESC
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        searchOverlay.style.display = 'none';
+    }
 })
