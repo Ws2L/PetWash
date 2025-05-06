c5b6a2d34522ab51b3347a7f103a3b080801f4ff
Ws2L <souza.weslley@aluno.ifsp.edu.br>
2025-05-06 17:58:29 -0300

    Criando galeria lightbox

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -42,3 +42,22 @@ document.addEventListener('keydown', (e) => {
         searchOverlay.style.display = 'none';
     }
 })
+
+//Galeria
+const imagens = document.querySelectorAll('.galeria img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+console.log(imagens);
+
+imagens.forEach((img) => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.classList.add('show') // funciona
+    })
+})
+
+//esconder o lightbox
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('show');
+})
